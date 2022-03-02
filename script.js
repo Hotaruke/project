67c812d409d69ef09b727deec926c5b98d1c5b88
Hotaru <example@gmail.com>
2022-03-02 18:05:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`ПРивет, ${user}`);
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
